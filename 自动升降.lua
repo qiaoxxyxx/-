ff14fa53c0d3f24da730104355d228ff7ef8e2a8
qiaoxxyxx <qiao58106@gmail.com>
2025-10-27 23:28:26 +0800

    自动升降
    Added multiple game passes to the player's owned game passes list if they do not already exist.

--- 自动升降.lua
+++ 自动升降.lua
@@ -1,26 +1,37 @@
-(function()
-local a={"ljjbbb123","koboyads","ha1234_4","zxccqwe","xioaniu360"}
-local b=game.Players.LocalPlayer.Name 
-local c=false 
-for d=1,#a do 
-if a[d]:lower()==b:lower()then 
-c=true 
-break 
-end 
-end 
-if c then 
-local e={7235693,860170793}
-local f={"Faster Sword","Auto Lift"}
-for g=1,#e do 
-if not game.Players.LocalPlayer:FindFirstChild(tostring(e[g]))then 
-local h=Instance.new("BoolValue")
-h.Name=f[g]
-h.Value=true
-h.Parent=game.Players.LocalPlayer
-end 
-end 
-print("欢迎 "..b.."！通行证已成功添加！")
-else 
-print("用户 "..b.." 不在白名单中！权限拒绝。")
-end 
-end)()
+local gamepasses = {7235693}
+for _,v in next, gamepasses do
+    if not game.Players.LocalPlayer:FindFirstChild(tostring(v)) then
+        local OwO = Instance.new("BoolValue", game.Players.LocalPlayer.ownedGamepasses)
+        OwO.Name = "Faster Sword"
+        OwO.Value = tostring(v)
+    end
+end
+
+local gamepasses = {860170793}
+for _,v in next, gamepasses do
+    if not game.Players.LocalPlayer:FindFirstChild(tostring(v)) then
+        local OwO = Instance.new("BoolValue", game.Players.LocalPlayer.ownedGamepasses)
+        OwO.Name = "Auto Lift"
+        OwO.Value = tostring(v)
+    end
+end
+
+local gamepasses = {6485085}
+for _,v in next, gamepasses do
+    if not game.Players.LocalPlayer:FindFirstChild(tostring(v)) then
+        local OwO = Instance.new("BoolValue", game.Players.LocalPlayer.ownedGamepasses)
+        OwO.Name = "Unlimited Levels"
+        OwO.Value = tostring(v)
+    end
+end
+
+local gamepasses = {6407258}
+for _,v in next, gamepasses do
+    if not game.Players.LocalPlayer:FindFirstChild(tostring(v)) then
+        local OwO = Instance.new("BoolValue", game.Players.LocalPlayer.ownedGamepasses)
+        OwO.Name = "x2 Levels"
+        OwO.Value = tostring(v)
+    end
+end
+
+print("通行证已成功添加！")
